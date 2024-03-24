f07ca955889a508cbf683fb1b681e3189193b16b
Deska1122 <164618453+Deska1122@users.noreply.github.com>
2024-03-24 22:57:53 +0600

    Add files via upload

---
+++ Crossvalid/cross.py
@@ -0,0 +1,17 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+
+data = load_breast_cancer()
+X = data.data
+y = data.target
+
+knn = KNeighborsClassifier(n_neighbors=7)
+
+scores = cross_val_score(knn, X, y, cv=10, scoring='accuracy')
+
+print("Точность для каждого фолда:")
+for i, score in enumerate(scores):
+    print(f"Фолд {i+1}: {score}")
+
+print("Средняя точность: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
